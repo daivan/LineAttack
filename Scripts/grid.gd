c64dd978c62b44b599ac4b20113691dd2ac62d51
Daivan Trinh <daivan@gmail.com>
2020-07-10 23:44:11 +0200

    Can now play the game

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -7,7 +7,7 @@ export (int) var height;
 export (int) var x_start;
 export (int) var y_start;
 export (int) var offset;
-
+export (int) var y_offset;
 
 # The piece array
 var possible_pieces = [
@@ -23,6 +23,8 @@ var all_pieces = [];
 # Tourch variables
 var first_touch = Vector2(0 ,0);
 var final_touch = Vector2(0 ,0);
+var controlling = false;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -68,7 +70,7 @@ func match_at(i, j, color):
 		if all_pieces[i][j - 1] != null && all_pieces[i][j-2] != null:
 			if all_pieces[i][j - 1].color == color && all_pieces[i][j - 2].color == color:
 				return true;
-	pass;
+	return false;
 
 func grid_to_pixel(column, row):
 	var new_x = x_start + offset * column;
@@ -81,16 +83,128 @@ func pixel_to_grid(pixel_x, pixel_y):
 	return Vector2(new_x, new_y);
 	pass;
 
-func touch_input():
-	if Input.is_action_just_pressed("ui_touch"):
-		first_touch = get_global_mouse_position();
-		var grid_position = pixel_to_grid(first_touch.x, first_touch.y);
-		print(grid_position);
-	if Input.is_action_just_released("ui_touch"):
-		final_touch = get_global_mouse_position();
-		
+func is_in_grid(grid_position):
+	if grid_position.x >= 0 && grid_position.x < width:
+		if grid_position.y >= 0 && grid_position.y < height:
+			return true;
+	return false;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func touch_input():
+	
+	if Input.is_action_just_pressed("ui_touch"):
+		if is_in_grid(pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y)):
+			first_touch = pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y);
+			controlling = true;
+		
+	if Input.is_action_just_released("ui_touch"):
+		if is_in_grid(pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y)) && controlling:
+			controlling = false;
+			final_touch = pixel_to_grid(get_global_mouse_position().x, get_global_mouse_position().y);
+			touch_difference(first_touch, final_touch);
+			
+			
+			
+func swap_pieces(column, row, direction):
+	var first_piece = all_pieces[column][row];
+	var other_piece = all_pieces[column + direction.x][row + direction.y];
+	if first_piece != null && other_piece != null:
+		all_pieces[column][row] = other_piece;
+		all_pieces[column + direction.x][row + direction.y] = first_piece;
+		first_piece.move(grid_to_pixel(column + direction.x, row + direction.y));
+		other_piece.move(grid_to_pixel(column, row));
+		find_matches();
+
+func touch_difference(grid_1, grid_2):
+	var difference = grid_2 - grid_1;
+	if abs(difference.x) > abs(difference.y):
+		if difference.x > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(1,0));
+		elif difference.x < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(-1,0));
+	elif abs(difference.y) > abs(difference.x):
+		if difference.y > 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0,1));
+		elif difference.y < 0:
+			swap_pieces(grid_1.x, grid_1.y, Vector2(0,-1));		
+		
+		
 func _process(delta):
 	touch_input();
 #	pass
+
+func find_matches():
+	for i in width:
+		for j in height:
+			if all_pieces[i][j] != null:
+				var current_color = all_pieces[i][j].color;
+				if i> 0 && i < width - 1:
+					if all_pieces[i-1][j] != null && all_pieces[i+1][j] != null:
+						if current_color == all_pieces[i + 1][j].color && current_color == all_pieces[i - 1][j].color:
+							all_pieces[i + 1][j].matched = true;
+							all_pieces[i + 1][j].dim();
+							all_pieces[i][j].matched = true;
+							all_pieces[i][j].dim();
+							all_pieces[i - 1][j].matched = true;
+							all_pieces[i - 1][j].dim();
+				if j> 0 && j < height - 1:
+					if all_pieces[i][j+1] != null && all_pieces[i][j-1] != null:
+						if current_color == all_pieces[i][j+1].color && current_color == all_pieces[i][j-1].color:
+							all_pieces[i][j+1].matched = true;
+							all_pieces[i][j+1].dim();
+							all_pieces[i][j].matched = true;
+							all_pieces[i][j].dim();
+							all_pieces[i][j-1].matched = true;
+							all_pieces[i][j-1].dim();
+							
+	get_parent().get_node("destroy_timer").start();
+								
+	
+func destroy_matched():
+	for i in width:
+		for j in height:
+			if all_pieces[i][j] != null:
+				if all_pieces[i][j].matched:
+					all_pieces[i][j].queue_free();
+					all_pieces[i][j] = null;
+	get_parent().get_node("collapse_timer").start();
+
+func collapse_columns():
+	for i in width:
+		for j in height:
+			if all_pieces[i][j] == null:
+				for k in range(j + 1, height):
+					if all_pieces[i][k] != null:
+						all_pieces[i][k].move(grid_to_pixel(i,j));
+						all_pieces[i][j] = all_pieces[i][k];
+						all_pieces[i][k] = null;
+						break;
+	get_parent().get_node("refill_timer").start();
+	
+func refill_columns():
+	for i in width:
+		for j in height:
+			if all_pieces[i][j] == null:
+				var rand = floor(rand_range(0, possible_pieces.size()));
+				var piece = possible_pieces[rand].instance();
+				var loops = 0;
+				while(match_at(i, j, piece.color) && loops < 100):
+					rand = floor(rand_range(0, possible_pieces.size()));
+					loops += 1;
+					piece = possible_pieces[rand].instance();
+				# instance that piece from the array
+				
+				add_child(piece);
+				piece.position = grid_to_pixel(i, j - y_offset);
+				piece.move(grid_to_pixel(i, j));
+				all_pieces[i][j] = piece;	
+
+
+func _on_destroy_timer_timeout():
+	destroy_matched();
+
+func _on_collapse_timer_timeout():
+	collapse_columns();
+
+func _on_rifill_timer_timeout():
+	refill_columns();
+
