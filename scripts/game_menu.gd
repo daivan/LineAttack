bdbb98dadce63ebe3b2386fd5d9a35a4ac6d40dd
Daivan Trinh <daivan@gmail.com>
2020-09-20 21:10:45 +0200

    Added button and fixed bottom background ui

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -2,8 +2,6 @@ extends Control
 
 
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#main menu panel active
@@ -12,16 +10,25 @@ func _ready():
 
 
 func _on_Main_settings_pressed():
+	print('hejsan44');
 	$Main.slide_out()
 	$Settings.slide_in()
 	
 
 
 func _on_Settings_back_button():
+	print('hejsan22');
 	$Main.slide_in()
 	$Settings.slide_out()
 
 
 func _on_Main_play_pressed():
 	get_tree().change_scene("res://scenes/game.tscn");
+	pass # Replace with function body.
+
+
+func _on_Main_play_time_attack_pressed():
+	get_tree().change_scene("res://scenes/time_attack.tscn");
+	print('hejsan');
+	#get_tree().change_scene("");
 	pass # Replace with function body.
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends "res://scripts/base_menu_panel.gd"
 
 signal play_pressed
 signal settings_pressed
-
+signal play_time_attack_pressed
 
 func _on_button1_pressed():
 	emit_signal("play_pressed")
@@ -10,3 +10,6 @@ func _on_button1_pressed():
 func _on_button2_pressed():
 	emit_signal("settings_pressed")
 
+func _on_button_15_pressed():
+	emit_signal("play_time_attack_pressed")
+	pass # Replace with function body.
