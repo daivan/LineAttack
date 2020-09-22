ae8619a298f7c1ce303dec15891fd2c16a19af3e
Daivan Trinh <daivan@gmail.com>
2020-09-23 00:55:51 +0200

    Added level_description

---
+++ scripts/level_description_screen.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+signal start_level
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_TextureButton_pressed():
+	$AnimationPlayer.play("slide_out");
+	emit_signal("start_level");
+	pass # Replace with function body.
