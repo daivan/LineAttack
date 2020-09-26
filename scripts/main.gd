8c51ca8116da849836a80dde3b3ed5edd2e6390d
Daivan Trinh <daivan@gmail.com>
2020-09-27 00:06:44 +0200

    Removed the button in game menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,7 +9,3 @@ func _on_button1_pressed():
 
 func _on_button2_pressed():
 	emit_signal("settings_pressed")
-
-func _on_button_15_pressed():
-	emit_signal("play_time_attack_pressed")
-	pass # Replace with function body.
