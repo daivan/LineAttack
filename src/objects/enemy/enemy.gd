8a4e2c4cabdb1bbf8f671ab038378e8ccf2bb50b
Daivan Trinh <daivan@gmail.com>
2020-10-15 20:46:27 +0200

    Added enemy_holder and enemies

---
+++ src/objects/enemy/enemy.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var health = 100;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func checkTakeDamage(i, j):
+	# Take damage
+	self.takeDamage(i, j);
+
+func takeDamage(i, j):
+	self.health = self.health - 10;
+	isDead();
+	
+func isDead():
+	if(health<1):
+		return true
+
+func tempDisplayHealth():
+	print(health);
