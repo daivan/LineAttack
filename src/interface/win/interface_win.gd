80842ea099ccf51d1d795d381942ea5a7d27a081
Daivan Trinh <daivan@gmail.com>
2020-10-25 21:56:54 +0100

    Can go to map from win screen

--- src/interface/win/interface_win.gd
+++ src/interface/win/interface_win.gd
@@ -9,3 +9,7 @@ func _on_ContinueButton_pressed():
 func _on_goal_holder_game_won():
 	slide_in()
 	pass # Replace with function body.
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("res://src/levels/level_select/level_select.tscn");
