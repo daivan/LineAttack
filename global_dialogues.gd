d6c1140d9a1548c834954a11f7feb274402b5c5c
Daivan Trinh <daivan@gmail.com>
2020-11-07 23:06:49 +0100

    Updated the dialogue

---
+++ global_dialogues.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+var dialogue_array = [
+	{
+		name = "Jack",
+		image = "res://src/resources/characters/character1.png",
+		position = 'left',
+		text = 'WOW WOW Wetiline text'
+	},
+	{
+		name = "daivan",
+		image = "res://src/resources/characters/character2.png",
+		position = 'right',
+		text = 'WOW WOW tiline text 2222'
+	},
+	{
+		name = "Jack",
+		image = "res://src/resources/characters/character1.png",
+		position = 'left',
+		text = 'WOW WOW iline text 3333'
+	},					
+]
--- src/scenes/dialogue/scene_dialogue.gd
+++ src/scenes/dialogue/scene_dialogue.gd
@@ -8,16 +8,56 @@ export (String) var level_to_load
 
 export (String, MULTILINE) var dialogue
 
+export(Array) var dialogue_objects
+
+var dialogue_array = [
+	{
+		name = "Jack",
+		image = "res://src/resources/characters/character1.png",
+		position = 'left',
+		text = 'WOW WOW Wetiline text'
+	},
+	{
+		name = "daivan",
+		image = "res://src/resources/characters/character2.png",
+		position = 'right',
+		text = 'WOW WOW tiline text 2222'
+	},
+	{
+		name = "Jack",
+		image = "res://src/resources/characters/character1.png",
+		position = 'left',
+		text = 'WOW WOW iline text 3333'
+	},					
+]
+
+var dialogue_step = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$RichTextLabel.text = dialogue
+
+	display_from_information_from_object(dialogue_array[dialogue_step]);
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_TextureButton_pressed():
-	get_tree().change_scene(level_to_load);
+	
+	dialogue_step += 1;
+	print(dialogue_step)
+	if dialogue_array.size() > dialogue_step:
+		display_from_information_from_object(dialogue_array[dialogue_step])
+	else:
+		print('Show play screen');
+
+
+
+func display_from_information_from_object(dialogue):
+	# Set the dialogue text
+	print(dialogue)
+	$RichTextLabel.text = dialogue.text;
+	
+	$name_background/name_label.text = dialogue.name;
+	$character.texture = load(dialogue.image);
+	if dialogue.position == 'left':
+		$character.flip_h = 0
+	else:
+		$character.flip_h = 1
